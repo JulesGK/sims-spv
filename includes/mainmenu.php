5a324f31603b2fd846b3fb063557d5bc10ae5cf9
JulesGK <83475540+JulesGK@users.noreply.github.com>
2022-09-14 22:42:50 +0200

    boilerplate

---
+++ includes/mainmenu.php
@@ -0,0 +1,18 @@
+<header class="main-header">
+  <nav class="main-nav">
+    <div class="container">
+      <a href="index.php" class="main-nav-logo">SPV</a>
+      <ul class="main-nav-items">
+        <li class="main-nav-item">
+          <a href="index.php" class="main-nav-link">Home</a>
+        </li>
+        <li class="main-nav-item">
+          <a href="#" class="main-nav-link">Home</a>
+        </li>
+        <li class="main-nav-item">
+          <a href="#" class="main-nav-link">Home</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
