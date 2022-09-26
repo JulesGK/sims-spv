f907b29af4acc4a605ccf5f34e525949bc39b01b
NorenAnton <112630207+NorenAnton@users.noreply.github.com>
2022-09-26 13:47:36 +0200

    Update and rename functions.js to main.js

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+//Loads the whole page
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+    const education = '';
+    const studylength = '';
+    const csnAllowence = '';
+    const csnLoan = '';
+    const job = '';
+    const servicePension = new Boolean();
+    var scen1BruSalary = '';
+    var scen1NetSalary = '';
+    var scen1Saving = '';
+    var scen2BruSalary = '';
+    var scen2NetSalary = '';
+    var scen2Saving = '';
+    var scen3BruSalary = '';
+    var scen3NetSalary = '';
+    var scen3Saving = '';
+
+
+    function input(){
+
+    }
+
+    function results() {
+
+    }
+
+    function graphs() {
+
+    }
+});
