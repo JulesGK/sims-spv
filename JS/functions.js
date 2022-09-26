992ed882132726090cf5fdea6d4ae056f03336b5
NorenAnton <112630207+NorenAnton@users.noreply.github.com>
2022-09-26 13:16:37 +0200

    Update functions.js

--- JS/functions.js
+++ JS/functions.js
@@ -1,17 +1,17 @@
 function variables() {
-    static const education;
-    static const studylength;
-    static const csnAllowence;
-    static const csnLoan;
-    static const job;
-    static const servicePension = new Boolean();
-    static const scen1BruSalary;
-    static const scen1NetSalary;
-    static const scen1Saving;
-    static const scen2BruSalary;
-    static const scen2NetSalary;
-    static const scen2Saving;
-    static const scen3BruSalary;
-    static const scen3NetSalary;
-    static const scen3Saving;
+    const education;
+    const studylength;
+    const csnAllowence;
+    const csnLoan;
+    const job;
+    const servicePension = new Boolean();
+    const scen1BruSalary;
+    const scen1NetSalary;
+    const scen1Saving;
+    const scen2BruSalary;
+    const scen2NetSalary;
+    const scen2Saving;
+    const scen3BruSalary;
+    const scen3NetSalary;
+    const scen3Saving;
 }
