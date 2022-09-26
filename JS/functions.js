b9bcdffdaaa342ec7acee79881e16124cae574b7
NorenAnton <112630207+NorenAnton@users.noreply.github.com>
2022-09-26 12:53:31 +0200

    Add files via upload

---
+++ JS/functions.js
@@ -0,0 +1,17 @@
+function variables() {
+    const education;
+    const studylength;
+    const csnAllowence;
+    const csnLoan;
+    const job;
+    const servicePension = new Boolean();
+    const scen1BruSalary;
+    const scen1NetSalary;
+    const scen1Saving;
+    const scen2BruSalary;
+    const scen2NetSalary;
+    const scen2Saving;
+    const scen3BruSalary;
+    const scen3NetSalary;
+    const scen3Saving;
+}
