8b8ad56baf6afe2fb0802dc9453ddef17413d9ae
ArKh2000 <74962495+ArKh2000@users.noreply.github.com>
2022-10-17 10:50:01 +0200

    Update jobs.class.php

--- includes/jobs.class.php
+++ includes/jobs.class.php
@@ -1,6 +1,7 @@
 <?php
 // Open connection to the database
-$db = mysqli_connect("localhost", "admin", "password", "admin");
+//$db = mysqli_connect("localhost", "admin", "password", "admin");
+$db= mysqli_connect('studentmysql.miun.se', 'arkh2000', 'qc18cg6j', 'arkh2000');
 
 
 if (mysqli_connect_errno()) {
