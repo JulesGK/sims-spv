d91cdd6776c1f95a68d26952c7822c0135ea8db8
JulesGK <83475540+JulesGK@users.noreply.github.com>
2022-10-27 00:28:18 +0200

    remove owl since is no longer used

--- assets/js/pyramid.js
+++ assets/js/pyramid.js
@@ -4,21 +4,4 @@ $(document).ready(function(){
         const servicePensionFigExtended = servicePensionFigWrapper.find('.service-pension-fig-extended');
         servicePensionFigExtended.toggleClass('show');
     });
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 4
-            }
-        }
-    })
 });
